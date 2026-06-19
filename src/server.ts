8b1eea6c43d8574745053a0f3da9b9660faae7a6
Rignu1503 <rigoberto.miranda1503@gmail.com>
2026-06-18 22:45:00 -0500

    fix(server) update serve.ts

--- src/server.ts
+++ src/server.ts
@@ -4,28 +4,19 @@ import {
   isMainModule,
   writeResponseToNodeResponse,
 } from '@angular/ssr/node';
-import express from 'express';
-import { join } from 'node:path';
 
-const browserDistFolder = join(import.meta.dirname, '../browser');
+import express from 'express';
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const browserDistFolder = join(__dirname, '../browser');
 
 const app = express();
 const angularApp = new AngularNodeAppEngine();
 
 /**
- * Example Express Rest API endpoints can be defined here.
- * Uncomment and define endpoints as necessary.
- *
- * Example:
- * ```ts
- * app.get('/api/{*splat}', (req, res) => {
- *   // Handle API request
- * });
- * ```
- */
-
-/**
- * Serve static files from /browser
+ * Archivos estáticos
  */
 app.use(
   express.static(browserDistFolder, {
@@ -36,33 +27,34 @@ app.use(
 );
 
 /**
- * Handle all other requests by rendering the Angular application.
+ * Render SSR Angular
  */
 app.use((req, res, next) => {
   angularApp
     .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
+    .then((response) => {
+      if (response) {
+        return writeResponseToNodeResponse(response, res);
+      }
+      next();
+    })
     .catch(next);
 });
 
 /**
- * Start the server if this module is the main entry point.
- * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
+ * Iniciar servidor
  */
 if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
-    if (error) {
-      throw error;
-    }
+  const port = Number(process.env['PORT']) || 4000;
 
-    console.log(`Node Express server listening on http://localhost:${port}`);
+  app.listen(port, () => {
+    console.log(
+      `🚀 Angular SSR ejecutándose en http://localhost:${port}`,
+    );
   });
 }
 
 /**
- * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
+ * Handler para Angular CLI
  */
 export const reqHandler = createNodeRequestHandler(app);
